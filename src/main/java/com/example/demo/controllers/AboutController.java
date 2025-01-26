a8627db8321692373e9bedaa866e22c74fc77b92
Kate Favre <katielfavre@gmail.com>
2025-01-25 20:58:23 -0800

    Part D

--- src/main/java/com/example/demo/controllers/AboutController.java
+++ src/main/java/com/example/demo/controllers/AboutController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutController {
-    @GetMapping("/about")
-    public String aboutPage(Model model) {
-        model.addAttribute("pageTitle", "About Us");
-        return "about";
-    }
+   @GetMapping("/about")
+   public String aboutPage(Model model) {
+       model.addAttribute("pageTitle", "About Us");
+       return "about";
+   }
 }
