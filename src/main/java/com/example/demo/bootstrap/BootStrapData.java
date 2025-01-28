7ecf17d20abc3920ab4839298090dae690a1dad0
Kate Favre <katielfavre@gmail.com>
2025-01-27 21:28:30 -0800

    added inhouseparts and updates to README.md

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,5 +1,6 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
@@ -28,75 +29,97 @@ public class BootStrapData implements CommandLineRunner {
 
     private final PartRepository partRepository;
     private final ProductRepository productRepository;
-
+    private final InhousePartRepository inhousePartRepository;
     private final OutsourcedPartRepository outsourcedPartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, InhousePartRepository inhousePartRepository, OutsourcedPartRepository outsourcedPartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
         this.outsourcedPartRepository=outsourcedPartRepository;
+        this.inhousePartRepository=inhousePartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        if (outsourcedPartRepository.count() == 0) {
-            OutsourcedPart strings = new OutsourcedPart();
-            strings.setCompanyName("Guitar Shop");
+        /*
+            InhousePart strings = new InhousePart();
             strings.setName("Guitar Strings");
             strings.setInv(40);
             strings.setPrice(10.0);
             strings.setId(100L);
+            inhousePartRepository.save(strings);
+            InhousePart thePart = null;
+            List<InhousePart> inhouseParts = (List<InhousePart>) inhousePartRepository.findAll();
+            for(InhousePart part:inhouseParts){
+                if(part.getName().equals("Guitar Strings"))thePart = part;
+            }
 
-            OutsourcedPart headstock = new OutsourcedPart();
-            headstock.setCompanyName("Guitar Shop");
+            InhousePart headstock = new InhousePart();
             headstock.setName("Headstock");
             headstock.setInv(15);
             headstock.setPrice(40.0);
             headstock.setId(101L);
+            inhousePartRepository.save(headstock);
+            InhousePart thePart = null;
+            List<InhousePart> inhouseParts = (List<InhousePart>) inhousePartRepository.findAll();
+            for(InhousePart part:inhouseParts){
+                if(part.getName().equals("Headstock"))thePart = part;
+            }
+            */
+        List<InhousePart> inhouseParts = (List<InhousePart>) inhousePartRepository.findAll();
+        for(InhousePart part:inhouseParts){
+            System.out.println(part.getName()+" "+part.getId());
 
+        }
+
+            /*
             OutsourcedPart body = new OutsourcedPart();
-            body.setCompanyName("Guitar Shop");
-            body.setName("body");
-            body.setInv(30);
-            body.setPrice(60.0);
+            body.setCompanyName("International Guitar Shop");
+            body.setName("Body");
+            body.setInv(40);
+            body.setPrice(10.0);
             body.setId(102L);
+            outsourcedPartRepository.save(body);
+            OutsourcedPart theOutPart = null;
+            List<OutsourcedPart> outsourcedParts = (List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            for(OutsourcedPart part:outsourcedParts){
+                if(part.getName().equals("Body"))theOutPart = part;
+            }
 
             OutsourcedPart tuningPegs = new OutsourcedPart();
-            tuningPegs.setCompanyName("Guitar Shop");
+            tuningPegs.setCompanyName("International Guitar Shop");
             tuningPegs.setName("Tuning Pegs");
             tuningPegs.setInv(50);
             tuningPegs.setPrice(40.0);
             tuningPegs.setId(103L);
+            outsourcedPartRepository.save(tuningPegs);
+            OutsourcedPart theOutPart = null;
+            List<OutsourcedPart> outsourcedParts = (List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            for(OutsourcedPart part:outsourcedParts){
+                if(part.getName().equals("Tuning Pegs"))theOutPart = part;
+            }
 
             OutsourcedPart guitarKnobs = new OutsourcedPart();
-            guitarKnobs.setCompanyName("Guitar Shop");
+            guitarKnobs.setCompanyName("International Guitar Shop");
             guitarKnobs.setName("Guitar Knobs");
             guitarKnobs.setInv(50);
             guitarKnobs.setPrice(30.0);
             guitarKnobs.setId(104L);
-
-
-            outsourcedPartRepository.save(strings);
-            outsourcedPartRepository.save(headstock);
-            outsourcedPartRepository.save(body);
-            outsourcedPartRepository.save(tuningPegs);
             outsourcedPartRepository.save(guitarKnobs);
-        }
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
-        }
+            OutsourcedPart theOutPart = null;
+            List<OutsourcedPart> outsourcedParts = (List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            for(OutsourcedPart part:outsourcedParts){
+                if(part.getName().equals("Guitar Knobs"))theOutPart = part;
+            }
 
-        System.out.println(thePart.getCompanyName());
-
+        */
 
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
-        if (productRepository.count() == 0) {
+        /*
             Product acoustic = new Product("Acoustic", 150.0, 15);
             Product nylonAcoustic = new Product("Nylon String Acoustic", 175.0, 15);
             Product flyingVElectric = new Product("Flying V Electric", 375.0, 15);
@@ -107,7 +130,9 @@ public class BootStrapData implements CommandLineRunner {
             productRepository.save(flyingVElectric);
             productRepository.save(gibsonElectric);
             productRepository.save(stratocasterElectric);
-        }
+
+         */
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
